883c61ee1518f40deb67944132739d989720e9cb
Binduprajapati23 <binduprajapati2312@gmail.com>
2024-11-25 16:42:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_7_22012011142/PersonAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.mad_practical_7_22012011142
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mad_practical_7_22012011142.PersonAdapter.PersonViewHolder
+class PersonAdapter(private val persons: Array<Person>) :
+    RecyclerView.Adapter<PersonViewHolder>() {
+    class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.name)
+        val phoneTextView: TextView = itemView.findViewById(R.id.phone)
+        val emailTextView: TextView = itemView.findViewById(R.id.email)
+        val addressTextView: TextView = itemView.findViewById(R.id.address)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            PersonViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.listview_component,
+                parent, false)
+        return PersonViewHolder(view)
+    }
+    override fun getItemCount(): Int = persons.size
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        val person = persons[position]
+        holder.nameTextView.text = person.name
+        holder.phoneTextView.text = person.phoneNo
+        holder.emailTextView.text = person.emailId
+        holder.addressTextView.text = person.address
+    }}
